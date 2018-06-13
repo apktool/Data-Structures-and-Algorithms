7156b53fc76648448eec4f2bd9de5c0980a65607
apktool <apktoolOo@gmail.com>
2018-06-13 21:49:03 +0800

    Perfect Squares

---
+++ Java/src/main/java/leetcode/medium/PerfectSquares.java
@@ -0,0 +1,48 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+/**
+ * @author apktool
+ * @date 2018-06-13 20:59
+ * *
+ * Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+ *
+ * Example 1:
+ *
+ * Input: n = 12
+ * Output: 3
+ * Explanation: 12 = 4 + 4 + 4.
+ *
+ * Example 2:
+ *
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ */
+public class PerfectSquares {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; i - j * j >= 0; j++) {
+                min = Math.min(min, dp[i - j * j] + 1);
+            }
+
+            dp[i] = min;
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+
+        PerfectSquares haha = new PerfectSquares();
+        int result = haha.numSquares(n);
+        System.out.println(result);
+    }
+}
