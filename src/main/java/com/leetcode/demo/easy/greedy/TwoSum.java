2edb2ac899b3dfc7de3ccb5e6de8e916febc3a82
apktool <apktoolOo@gmail.com>
2021-03-22 08:19:16 +0800

    [ADD] two sum

---
+++ src/main/java/com/leetcode/demo/easy/greedy/TwoSum.java
@@ -0,0 +1,50 @@
+package com.leetcode.demo.easy.greedy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那两个整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class TwoSum {
+    public int[] solve01(int[] nums, int target) {
+        int[] res = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > i; j--) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public int[] solve02(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = target - nums[i];
+            if (map.containsKey(tmp)) {
+                res[0] = map.get(tmp);
+                res[1] = i;
+                return res;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/test/java/com/leetcode/demo/easy/greedy/TwoSumTest.java
@@ -0,0 +1,28 @@
+package com.leetcode.demo.easy.greedy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TwoSumTest {
+    @Test
+    public void solve01() {
+        TwoSum demo = new TwoSum();
+
+        int[] nums1 = {1, 2, 3, 4, 5};
+        int target1 = 6;
+        int[] res1 = {1, 3};
+        int[] res = demo.solve01(nums1, target1);
+        Assertions.assertArrayEquals(res1, res);
+    }
+
+    @Test
+    public void solve02() {
+        TwoSum demo = new TwoSum();
+
+        int[] nums1 = {1, 2, 3, 4, 5};
+        int target1 = 6;
+        int[] res1 = {1, 3};
+        int[] res = demo.solve02(nums1, target1);
+        Assertions.assertArrayEquals(res1, res);
+    }
+}
