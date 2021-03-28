eb397a1e607cc23bc84cb06bf4fedd521a911bd6
apktool <apktoolOo@gmail.com>
2021-03-28 10:03:23 +0800

    [ADD] best time to buy and sell stock

---
+++ src/main/java/com/leetcode/demo/easy/array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,34 @@
+package com.leetcode.demo.easy.array;
+
+/**
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * <p>
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ * <p>
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class BestTimeToBuyAndSellStock {
+
+    /**
+     * 贪心
+     * 取出最低峰的价格，然后获取最大的利润
+     *
+     * @param prices
+     * @return
+     */
+    public int solve01(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = Integer.MIN_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+}
