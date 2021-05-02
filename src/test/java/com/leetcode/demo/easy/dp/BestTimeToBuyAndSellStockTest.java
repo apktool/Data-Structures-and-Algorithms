e3166bf499a8dced70e8da2a88f0ad6d02d075fd
apktool <apktoolOo@gmail.com>
2021-05-02 23:14:12 +0800

    [ADD] best time to buy and sell stock

---
+++ src/test/java/com/leetcode/demo/easy/dp/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,16 @@
+package com.leetcode.demo.easy.dp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BestTimeToBuyAndSellStockTest {
+    @Test
+    public void solve01() {
+        BestTimeToBuyAndSellStock demo = new BestTimeToBuyAndSellStock();
+
+        int[] nums1 = {7, 1, 5, 3, 6, 4};
+        int expect1 = 5;
+        int res1 = demo.solve01(nums1);
+        Assertions.assertEquals(expect1, res1);
+    }
+}
