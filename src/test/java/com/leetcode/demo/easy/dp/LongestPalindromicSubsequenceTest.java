8d099df9a5cd5e7620f6d26fee532774663c60ed
apktool <apktoolOo@gmail.com>
2021-06-16 11:09:19 +0800

    [ADD] longest palindromic subsequence

---
+++ src/test/java/com/leetcode/demo/easy/dp/LongestPalindromicSubsequenceTest.java
@@ -0,0 +1,12 @@
+package com.leetcode.demo.easy.dp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestPalindromicSubsequenceTest {
+
+    @Test
+    void solve01() {
+    }
+}
